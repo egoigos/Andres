63413a04ab538f9efb2cf8528ceeb39a1080a56d
Andrés Antonietti <egoigos@hotmail.com>
2020-02-19 05:08:31 +0100

    Create loading.js

---
+++ loading.js
@@ -0,0 +1,59 @@
+function es(){
+    document.getElementById("de").innerHTML=    "Alemán";
+    document.getElementById("eng").innerHTML=   "Inglés";
+    document.getElementById("esp").innerHTML=   "Español";
+    document.getElementById("it").innerHTML=    "Italiano";
+    document.getElementById("val").innerHTML=   "Valenciano";
+
+    document.getElementById("title").innerHTML=     "<h2>Sitio en Costrucción</h2>";
+    document.getElementById("txt").innerHTML=       "<p>Lamento las molestias, estoy diseñando el contenido.<br />En breve estará disponible.</p><p>Mientras tanto puedes descargar el CV el botón correspondiente.</p>";
+    document.getElementById("btn").innerHTML=       '<a href="https://drive.google.com/open?id=1c4jyZqax_rO_lMuUxyKY3GNuh6SS1KZp">Descargar CV</a>';
+}
+
+function eng(){
+    document.getElementById("de").innerHTML=    "German";
+    document.getElementById("eng").innerHTML=   "English";
+    document.getElementById("esp").innerHTML=   "Spanish";
+    document.getElementById("it").innerHTML=    "Italian";
+    document.getElementById("val").innerHTML=   "Valencian";
+
+    document.getElementById("title").innerHTML=     "<h2>Under construction</h2>";
+    document.getElementById("txt").innerHTML=       "<p>Sorry for the inconvenience, I'm designing the content, I think it will be available shortly.</p><p>Meanwhile you can download the CV the corresponding button if would to.</p>";
+    document.getElementById("btn").innerHTML=       '<a href="https://drive.google.com/open?id=1_4MH8K4qsAJgiMnCVU0whbu1xm-nswbC">Download CV</a>';
+}
+
+function it(){
+    document.getElementById("de").innerHTML=    "Tedesco";
+    document.getElementById("eng").innerHTML=   "Inglese";
+    document.getElementById("esp").innerHTML=   "Spagnuolo";
+    document.getElementById("it").innerHTML=    "Italiano";
+    document.getElementById("val").innerHTML=   "Valenciano";
+
+    document.getElementById("title").innerHTML=     "<h2>Sito Web in costruzione</h2>";
+    document.getElementById("txt").innerHTML=       "<p>Ci scusiamo per l'inconveniente, sto pianificando il contenuto; sarà presto disponibile.</p><p>Nel frattempo, puoi scaricare il CV con il pulsante corrispondente, è in spagnolo, per la versione inglesa o italiana richiedilo a egoigos@hotmail.com, grazie mille.</p>";
+    document.getElementById("btn").innerHTML=       '<a href="https://drive.google.com/open?id=1c4jyZqax_rO_lMuUxyKY3GNuh6SS1KZp">Scarica il CV</a>';
+}
+
+function va(){
+    document.getElementById("de").innerHTML=    "Alemany";
+    document.getElementById("eng").innerHTML=   "Anglès";
+    document.getElementById("esp").innerHTML=   "Espanyol";
+    document.getElementById("it").innerHTML=    "Italià";
+    document.getElementById("val").innerHTML=   "Valencià";
+
+    document.getElementById("title").innerHTML=     "<h2>Lloc en construcció</h2>";
+    document.getElementById("txt").innerHTML=       "<p>Lament les molèsties, estic dissenyant el contingut.<br />En breu estarà disponible. </p><p>Mentrestant pots descarregar el CV el botó corresponent, moltes gràcies.</p>";
+    document.getElementById("btn").innerHTML=       '<a href="https://drive.google.com/open?id=1c4jyZqax_rO_lMuUxyKY3GNuh6SS1KZp">Descarregar CV</a>';
+}
+
+function de(){
+    document.getElementById("de").innerHTML=    "Deutsch";
+    document.getElementById("eng").innerHTML=   "Englisch";
+    document.getElementById("esp").innerHTML=   "Spanisch";
+    document.getElementById("it").innerHTML=    "Italienisch";
+    document.getElementById("val").innerHTML=   "Valencia";
+
+    document.getElementById("title").innerHTML=     "<h2>Seite im Aufbau</h2>";
+    document.getElementById("txt").innerHTML=       "<p> Entschuldigen Sie die Unannehmlichkeiten, ich entwerfe den Inhalt. <br />Es wird bald verfügbar sein. </p><p>In der Zwischenzeit können Sie den Lebenslauf über den entsprechenden Button herunterladen, vielen Dank.</p>";
+    document.getElementById("btn").innerHTML=       '<a href="https://drive.google.com/open?id=1NSWitc3Oabzp_1fkc-6JKUhVhsQZggL0">Herunterladen CV</a>';
+}
